74eb64d0295471f0f01f4f1b05162a5620da3628
ngocson18081195 <ngocson18081195@gmail.com>
2019-07-13 10:37:31 +0700

    Config CROS

---
+++ src/main/java/eurekaproduct/config/WebConfig.java
@@ -0,0 +1,19 @@
+package eurekaproduct.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Create by ngocson on 13/07/2019
+ */
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+}
