8ea89c97dd4e94fd21b2c3b2a0f02f42a13c0670
ngocson18081195 <ngocson18081195@gmail.com>
2019-07-13 11:12:22 +0700

    Log

--- src/main/java/eurekaproduct/config/SimpleCORSFilter.java
+++ src/main/java/eurekaproduct/config/SimpleCORSFilter.java
@@ -20,7 +20,7 @@ public class SimpleCORSFilter implements Filter {
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
-
+        log.info("SimpleCORSFilter init");
     }
 
     @Override
